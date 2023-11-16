6bb26965080fe00e06499f364550293ec242627f
haowong <haowong.seu@gmail.com>
2023-11-16 10:49:23 +0800

    update

--- 坐标转换/CoordinateTransformationCmd.cs
+++ 坐标转换/CoordinateTransformationCmd.cs
@@ -14,6 +14,7 @@ using ESRI.ArcGIS.AnalysisTools;
 using ESRI.ArcGIS.ConversionTools;
 using ESRI.ArcGIS.DataManagementTools;
 using ESRI.ArcGIS.DataSourcesGDB;
+using ESRI.ArcGIS.GeoAnalyst;
 using ESRI.ArcGIS.Geoprocessor;
 using SMGI.Plugin.DCDProcess;
 
@@ -26,10 +27,11 @@ namespace SMGI.Plugin.CollaborativeWorkWithAccount
         {
             get
             {
-                return m_Application != null;
+                return m_Application != null && m_Application.Workspace != null;
             }
         }
 
+        private double mapScale = 0.0;
         private double R = 6371116;
         private double mu0 = 1 / 26000000;
         private double x0;
@@ -86,10 +88,11 @@ namespace SMGI.Plugin.CollaborativeWorkWithAccount
             return an * l / ln;
         }
 
+        // L：纬度（坐标）；B：经度（坐标）
         void multiConicProjection(double[] x, double[] y, double L, double B, double midlL, double mapScale)
         {
-            huL = L * Math.PI / 180;
-            huB = B * Math.PI / 180;
+            huL = L * Math.PI / 180; //纬度（弧度）
+            huB = B * Math.PI / 180; //经度（弧度）
 
             x0 = getx0();
             xn = getxn();
@@ -104,7 +107,7 @@ namespace SMGI.Plugin.CollaborativeWorkWithAccount
                 an = getan();
                 l = getl(L, midlL);
                 a = geta();
-                
+
                 //MessageBox.Show(" q: " + q + " sinan: " + sinan + " an: " + an + " l: " + l + " a: " + a, "中间结果2");
                 //MessageBox.Show((q * Math.Sin(a) * 14000).ToString(), "中间结果3");
 
@@ -131,8 +134,222 @@ namespace SMGI.Plugin.CollaborativeWorkWithAccount
 
         public override void OnClick()
         {
-            multiConicProjection(x, y, 324, 33, 150, 1100);
-            MessageBox.Show("x[0]: " + x[0] + " y[0]: " + y[0]);
+
+            mapScale = m_Application.MapControl.Map.ReferenceScale;
+
+            if (mapScale == 0)
+            {
+                MessageBox.Show("请先设置参考比例尺！");
+                return;
+            }
+
+            //multiConicProjection(x, y, 324, 33, 150, mapScale);
+            //MessageBox.Show("x[0]: " + x[0] + " y[0]: " + y[0]);
+
+            using (var wo = m_Application.SetBusy())
+            {
+                GeoDBDataTransfer(m_Application.Workspace.EsriWorkspace, wo);
+            }
+
+        }
+
+        public void GeoDBDataTransfer(IWorkspace ws, WaitOperation wo)
+        {
+            IFeatureWorkspace fws = ws as IFeatureWorkspace;
+            Dictionary<string, IFeatureClass> fcName2FC = DCDHelper.GetAllFeatureClassFromWorkspace(fws);
+            // 获取数据库中要素类的数量
+            int fcTotalNum = fcName2FC.Count;
+            int fcNum = 0;
+            foreach (var kv in fcName2FC)
+            {
+                fcNum++;
+                IFeatureClass fc = kv.Value;
+                String fcname = kv.Key;
+
+                ISpatialReference ISR = (fc as IGeoDataset).SpatialReference;
+                esriGeometryType geometryType = fc.ShapeType;
+
+                FeatureClassTransfer(kv, fws, geometryType, ISR, fcNum, fcTotalNum, wo);
+            }
+
+        }
+
+        public void FeatureClassTransfer(KeyValuePair<string, IFeatureClass> fcName2FC, IFeatureWorkspace fws, esriGeometryType geometryType, ISpatialReference ISR, int fcNum, int fcTotalNum, WaitOperation wo)
+        {
+            IFeatureClass fc = fcName2FC.Value;
+            String fcname = fcName2FC.Key;
+
+            // 获取要素类中要素的数量
+            int featureCount = fc.FeatureCount(null); // 如果传入 null，则计算所有的要素数量
+            IFeatureClassManage featureClassManage = (IFeatureClassManage)fc;
+
+            // 设置为未知坐标系统
+            ISpatialReference unknownSpatialReference = new UnknownCoordinateSystem() as ISpatialReference;
+
+            IFeature feature;
+            IGeometry pGeo;
+            IPoint point;
+            IPointCollection pointCollection;
+            int pointCount;
+
+            int featurecount = 0;
+
+            IFeatureCursor featureCursor = null;
+            try
+            {
+                featureCursor = fc.Search(null, false);
+
+                while ((feature = (IFeature)featureCursor.NextFeature()) != null)
+                {
+                    featurecount++;
+                    pGeo = feature.Shape;
+                    wo.SetText("正在处理第" + fcNum + "/" + fcTotalNum + "个要素类" + fcname + "的第" + featurecount + "/" + featureCount + "个要素");
+                    Console.WriteLine("正在处理第" + fcNum + "/" + fcTotalNum + "个要素类" + fcname + "的第" + featurecount + "/" + featureCount + "个要素");
+                    //pGeo.SpatialReference = unknownSpatialReference;
+
+                    // 不考虑几何为空的要素
+                    if (pGeo == null || pGeo.IsEmpty)
+                    {
+                        continue;
+                    }
+
+                    // 根据几何类型输出相应信息
+                    switch (geometryType)
+                    {
+                        case esriGeometryType.esriGeometryPoint:
+
+                            // 获取点要素的几何对象
+                            point = pGeo as IPoint;
+
+                            double longitude = point.X;
+                            double latitude = point.Y;
+                            multiConicProjection(x, y, latitude, longitude, 150, mapScale);
+
+                            // 设置点要素的坐标
+                            point.PutCoords(longitude + 2, latitude + 2);
+                            //pGeo.Project(ISR);
+
+                            // 更新要素
+                            feature.Shape = point;
+                            feature.Store();
+                            break;
+                        case esriGeometryType.esriGeometryPolyline:
+                            // 获取要素的所有点
+                            pointCollection = pGeo as IPointCollection;
+                            pointCount = pointCollection.PointCount;
+
+
+                            // 对于每个点，获取其当前的横纵坐标，然后进行移动
+                            for (int i = 0; i < pointCount; i++)
+                            {
+                                point = pointCollection.get_Point(i);
+                                double x1 = point.X;
+                                double y1 = point.Y;
+
+                                // 移动点的横纵坐标
+                                point.X = x1 + 2; 
+                                point.Y = y1 + 2; 
+
+                                // 将移动后的点重新设置到要素中
+                                pointCollection.UpdatePoint(i, point);
+                            }
+
+                            // 更新要素
+                            feature.Shape = pointCollection as IGeometry;
+                            feature.Store();
+                            break;
+                        case esriGeometryType.esriGeometryPolygon:
+                            IPolygon polygon = (IPolygon)pGeo;
+
+                            IGeometryCollection ringCollection = (IGeometryCollection)polygon;
+                            for (int i = 0; i < ringCollection.GeometryCount; i++)
+                            {
+                                IRing ring = (IRing)ringCollection.get_Geometry(i);
+                                pointCollection = (IPointCollection)ring;
+
+                                IPoint[] originalPoints = new IPoint[pointCollection.PointCount];
+
+                                // 记录原始点的位置，保证面不发生形变
+                                for (int j = 0; j < pointCollection.PointCount; j++)
+                                {
+                                    originalPoints[j] = pointCollection.get_Point(j);
+                                }
+
+                                // 移动所有点
+                                for (int j = 0; j < pointCollection.PointCount; j++)
+                                {
+                                    point = pointCollection.get_Point(j);
+
+                                    // 在这里应用你的坐标移动逻辑，例如：
+                                    double newX = originalPoints[j].X + 2;
+                                    double newY = originalPoints[j].Y + 2;
+
+                                    // 更新点的位置
+                                    point.X = newX;
+                                    point.Y = newY;
+
+                                    // 将移动后的点重新设置到环中
+                                    pointCollection.UpdatePoint(j, point);
+                                }
+                            }
+
+                            // 更新要素的几何对象
+                            feature.Shape = (IGeometry)polygon;
+                            feature.Store();
+                            break;
+                        /* 不对面形变做出处理
+                           case esriGeometryType.esriGeometryPolygon:
+                           IPolygon polygon = (IPolygon)pGeo;
+                           
+                           IGeometryCollection ringCollection = (IGeometryCollection)polygon;
+                           for (int i = 0; i < ringCollection.GeometryCount; i++)
+                           {
+                           IRing ring = (IRing)ringCollection.get_Geometry(i);
+                           pointCollection = (IPointCollection)ring;
+                           
+                           for (int j = 0; j < pointCollection.PointCount; j++)
+                           {
+                           point = pointCollection.get_Point(j);
+                           
+                           // 在这里应用你的坐标移动逻辑，例如：
+                           double newX = point.X + 2.0;
+                           double newY = point.Y + 2.0;
+                           
+                           // 更新点的位置
+                           point.X = newX;
+                           point.Y = newY;
+                           
+                           // 将移动后的点重新设置到环中
+                           pointCollection.UpdatePoint(j, point);
+                           }
+                           }
+                           
+                           // 更新要素的几何对象
+                           feature.Shape = (IGeometry)polygon;
+                           feature.Store();
+                           break;
+                         */
+                        default:
+                            MessageBox.Show("出现未知几何类型要素！", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            return;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Trace.WriteLine(ex.Message);
+                System.Diagnostics.Trace.WriteLine(ex.Source);
+                System.Diagnostics.Trace.WriteLine(ex.StackTrace);
+                throw;
+            }
+            finally
+            {
+                // 在 finally 块中释放 COM 对象
+                if (featureCursor != null)
+                {
+                    Marshal.ReleaseComObject(featureCursor);
+                }
+            }
         }
     }
 }
